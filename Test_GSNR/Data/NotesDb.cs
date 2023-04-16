1cc69f9d4082692b3de0c2655f7926cce65cc7ef
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-04-17 01:38:15 +0300

    Контекст БД

---
+++ Test_GSNR/Data/NotesDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_GSNR.Models;
+
+namespace Test_GSNR.Data;
+
+public class NotesDb : DbContext
+{
+	public NotesDb(DbContextOptions<NotesDb> options) : base(options) { }
+
+	public DbSet<NoteItem> Notes { get; set; }
+}
